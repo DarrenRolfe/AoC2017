5e2b0d7d1091d728ebf840c3411d5b180c7c8948
DarrenRolfe <34160154+DarrenRolfe@users.noreply.github.com>
2017-12-04 15:40:00 +0000

    Add files via upload

---
+++ AoC-Day3-1.ps1
@@ -0,0 +1,57 @@
+﻿$target = 312051
+#$target = 49
+$value = 1
+$incvalue = 1
+$incjumps = 0
+$a =""
+
+do {
+    $incjumps++
+    $oldvalue = $value
+    $value += $incvalue
+    Write-Host "JUMP $incjumps | INC: $incvalue | VALUE: $value"
+    $check = $incjumps/2
+    if ([int32]::TryParse($check,[ref]$a)) {
+        $incvalue++
+    }
+} until ($value -gt $target)
+
+Write-Host "Target ($target) in jump $incjumps"
+
+
+
+$midwaysteps = [math]::Ceiling($incvalue/2)
+Write-Host "MIDWAY: $midwaysteps"
+
+if ($midwaysteps -ne $incvalue/2) {
+    $oldvalue--
+}
+
+$working = $oldvalue + $midwaysteps
+if ($working -gt $target) {
+    $steps = 0
+    do {
+        $target++
+        $steps++
+    } until ($target -eq $working)
+} elseif ($working -eq $target) {
+    $steps = 0
+} else {
+    $steps = 0
+    do {
+        $target--
+        $steps++
+    } until ($target -eq $working)
+}
+
+Write-Host "$steps steps along line"
+
+
+
+$directjumps = [math]::Ceiling(($incjumps-1)/4)
+
+Write-Host "$directjumps direct jumps"
+
+$total = $steps + $directjumps
+
+Write-Host "TOTAL: $total moves required"
---
+++ AoC-Day3-2.ps1
@@ -0,0 +1,25 @@
+﻿# array lengths ref previous arrays
+
+$target = 312051
+#$target = 49
+$value = 1
+$incvalue = 1
+$incjumps = 0
+$a = ""
+
+do {
+    $incjumps++
+    foreach ($step in $incvalue) {
+        $value = $jumpray[$step-1]
+        $jumpray[$step] = $value,
+    }
+    $check = $incjumps/2
+    if ([int32]::TryParse($check,[ref]$a)) {
+        $incvalue++
+    }
+} until ($value -gt $target)
+
+Write-Host "TARGET: $target | VALUE: $value | JUMP: $incjumps | JUMP INC: $incvalue"
+
+
+ if $incjumps
