e4da12d482862753b25344cc828e64e3ec4a750b
Rolfe <34160154+DarrenRolfe@users.noreply.github.com>
2017-12-08 06:04:46 +0000

    Add Day 8

---
+++ AoC-Day8-1.ps1
@@ -0,0 +1,19 @@
+﻿[string[]]$array = "b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10
+" -split "`n";
+
+[string[]]$var = @()
+[string[]]$vv = @()
+
+foreach ($line in $array) {
+    $var += ($line -split " ")[0].trim()
+    $vv += 0
+}
+
+$n = 0
+foreach ($line in $var) {
+    Write-Host $var[$n],":",$vv[$n]
+    $n++
+}
